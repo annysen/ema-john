11902dfbbe3f22da619e810805f5dfa3816665ab
annysen <annysen95@gmail.com>
2022-12-05 02:22:40 +0600

    add to db is added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import "./Cart.css"
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+    
+    let totalPrice= 0;
+    let shipping = 0;
+    
+    for ( const product of cart) {
+        totalPrice = totalPrice + product.price;
+        shipping = shipping + product.shipping;
+        }
+    const tax = parseFloat((totalPrice * 0.1).toFixed(2));
+    const grandTotal = (totalPrice + shipping + tax).toFixed(2);
+
     return (
-        <div>
+        <div className='cart-container'>
             <h1>Order Summery</h1>
             <h3>Total Item added: {cart.length}</h3>
+            <p>Total price: ${totalPrice}</p>
+            <p>Sipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <p>Grand price: ${grandTotal}</p>
 
         </div>
     );
